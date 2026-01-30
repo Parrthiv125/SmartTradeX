46368b53b3da1ae56f91d83410dedab4d01632af
Aeshvi Bhuva <aeshvi0704@gmail.com>
2026-01-30 16:08:46 +0530

    add placeholder chart component

---
+++ gui/components/chart.py
@@ -0,0 +1,12 @@
+# gui/components/chart.py
+
+import streamlit as st
+
+
+def render_chart(title: str = "Market Chart"):
+    """
+    Placeholder chart container.
+    Real chart logic will be added later.
+    """
+    st.subheader(title)
+    st.info("Chart will be displayed here.")
